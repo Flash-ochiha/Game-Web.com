2749e39ea3df9fad2b39525d67a7f267c2d07edf
Flash-ochiha <spongeboobsummer@gmail.com>
2024-12-18 22:59:32 +0000

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('startButton').addEventListener('click', function() {
+    alert('Game Starting! Get ready for an adventure in the Pixel Game World!');
+    // Here you can add more game logic or features
+});
